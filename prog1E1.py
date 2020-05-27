7438476bb2f0fd925d262aaeec58fae11fd9d602
Marina Neumann <marinaneumann2468@yahoo.com>
2020-05-27 15:38:14 -0700

    Fixed target values setting finallygit add Prog1E1.py

--- prog1E1.py
+++ prog1E1.py
@@ -12,7 +12,7 @@ def main():
 
 
     dataLoad() #Load data from files
-    #build(X_train, Y_train, X_test, Y_test)     #build NN and run epochs
+    build(X_train, Y_train, X_test, Y_test)     #build NN and run epochs
 
 #Data data --> MNISET
 def dataLoad():
@@ -49,8 +49,9 @@ def build(X_train, Y_train, X_test, Y_test):
     mnnetTrain = NN(X_train, Y_train)
 
     #for z in range(epochs):
-    mnnetTrain.propForward()
-    mnnetTrain.backprop()
+    for z in range(0,1):
+        mnnetTrain.propForward()
+        #mnnetTrain.backprop()
 
         #Shuffle data before next epoch?
         #permutation = np.random.permutation(X_train.shape[1])
@@ -77,6 +78,9 @@ class NN:
         self.mom = M   #allowing for user to choose momentum, by default will be 0.9
         self.createMatrices(numInputs, numHidden, outNum)
 
+        # self.deltaChangeK = np.ones()
+        # self.deltChangeJ = np.ones()
+
     def createMatrices(self,numInputs, numHidden, outNum):
         # inp_dim = x.shape[1]
         # out_dim = y.shape[0]
@@ -85,61 +89,60 @@ class NN:
         self.weightsKJ = np.random.uniform(-0.5, 0.5, size=(numHidden, outNum))
         self.bias1 = np.ones((1, numHidden))  # might need to change biases??
         self.bias2 = np.ones((1, outNum))
-        print("Bias1:", self.bias1)
-        print("Bias2: ", self.bias2)
-        # self.deltaChangeK = np.zeros()
-        #self.deltaChangeJ = np.zeros()
+        # print("Bias1:", self.bias1)
+        # print("Bias2: ", self.bias2)
+
+        self.deltaChangeK = np.zeros(len(self.weightsKJ))
+        self.deltChangeJ = np.zeros(len(self.weightsJI))
 
 # Propogate input forward
     def propForward(self) :
+        global tK
+        #tK = np.zeros(10, dtype=float)
+        tK = [[0.1 for j in range(10)] for i in range(len(self.y))]
+        #print("The target values before assignment:", tK)
+        index = 0
+        for z in tK:
+            z[self.y[index]] = 0.9
+            #print(z)
+            index +=1
+
+
+
+
         z = np.dot(self.x, self.weightsJI) + self.bias1
         self.hiddenL = sigmAct(z)
         d = np.dot(self.hiddenL, self.weightsKJ) + self.bias2
         self.outputs = sigmAct(d)
-        print("HiddenLayers: ", self.hiddenL)
-        print("Outputs: ", self.outputs)
+        # print("HiddenLayers: ", self.hiddenL)
+        # print("Outputs: ", self.outputs)
 
     def backprop(self):
         deltaK, deltaJ = self.errorcalc()
-        #print("This is deltaK", deltaK)
-        #print("This is deltaJ", deltaJ)
+        print("This is deltaK", deltaK)
+        print("This is deltaJ", deltaJ)
         #update weights w/ momentum
 
-        # deltaChangeK = (self.lr * delaK * self.hiddenL) + (self.mom *self.deltaChangeK)
-        # self.weightsKJ = self.weightsLJ + deltaChangeK
-        #
-        # deltaChangeJ = (self.lr * deltaJ * self.inputs) +(self.mom *self.deltaChangeJ)
-        # self.weightsJI = self.weightsJI + deltaChangeJ
+        deltaChangeK = (self.lr * deltaK * self.hiddenL) + (self.mom *self.deltaChangeK)
+        self.weightsKJ = self.weightsLJ + deltaChangeK
+
+        deltaChangeJ = (self.lr * deltaJ * self.inputs) +(self.mom *self.deltaChangeJ)
+        self.weightsJI = self.weightsJI + deltaChangeJ
 
 
 # Caluclate error
     def errorcalc(self):
-        global tK
-
-        i = 0
-        # for i in self.outputs:
-        #     if self.outputs.index(i) == self.y.index(i):
-        #         tK[i] = 0.9
-        #     else:
-        #         tK[i] = 0.1
-        # outputs = np.array(self.outputs).astype(int)
-        # labels = np.array(self.y).astype(int)
-        # for i in outputs:
-        #     if outputs[i] == labels[i]:
-        #         tK[i] = 0.9
-        #     else:
-        #         tK[i] = 0.1
-        #Figures out tK values depending on if input class is kth class or not.. don't think this is right
-        #for i in self.outputs:
-            #if self.outputs == self.y:
-                #tK[i] = 0.9
-            #else:
-                #tK[i] = 0.1
 
         deltaK = self.outputs * (1- self.outputs)* (tK - self.outputs)
         deltaJ = self.hiddenL * (1- self.hiddenL) * np.dot(self.weightsKJ, deltaK)
 
         return deltaK, deltaJ
 
+    def accuracyTesting(self):
+        count = 0
+        for i in self.outputs:
+            if i == self.y:
+                count +=1
 
+        return (count/len(self.x))*100
 main()
